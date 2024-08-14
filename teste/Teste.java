438246ddc8cbfd6091d3cc7a7e166c05258b6800
Brenokly <breno.moura@alunos.ufersa.edu.br>
2024-08-13 21:59:17 -0300

    update

--- teste/Teste.java
+++ teste/Teste.java
@@ -4,25 +4,90 @@ import tree.BinaryTree;
 
 public class Teste {
   public static void main(String[] args) {
-    BinaryTree<Integer> tree = new BinaryTree<>();
-    tree.insert(1);
-    tree.insert(2);
-    tree.insert(3);
-    tree.insert(4);
-    tree.insert(5);
-    tree.insert(6);
-    tree.insert(7);
+    BinaryTree tree = new BinaryTree();
+    tree.insert(11); // 1°
+    tree.insert(6); // 2°
+    tree.insert(13); // 3°
+    tree.insert(17); // 4°
+    tree.insert(4); // 5°
+    tree.insert(9); // 6°
+    tree.insert(7); // 7°
+    tree.insert(10); // 8°
+    tree.insert(16); // 9°
 
-    tree.displayOrder();
+    // tree.ordemRec();
 
-    tree.insert(8);
+    // tree.remove(11);
 
-    tree.displayOrder();
+    // tree.ordemRec();
 
-    tree.deleteRecursive(4);
+    // System.out.println("-------------------------------------------------------\n");
 
-    tree.displayOrder();
+    // tree.ordemRec();
+    // tree.ordemIte();
 
-    System.out.println("Dado encontrado: " + tree.search(3).getData());
+    // System.out.println("-------------------------------------------------------\n");
+
+    // tree.preOrdemRec();
+    // tree.preOrdemIte();
+
+    // System.out.println("-------------------------------------------------------\n");
+
+    // tree.posOrdemRec();
+    // tree.posOrdemIte();
+
+    // System.out.println("-------------------------------------------------------\n");
+
+    // ------------------------------------------------------------------------------------------------
+
+    System.out.println("Menor chave: " + tree.menorChave());
+
+    System.out.println("-------------------------------------------------------\n");
+
+    System.out.println("Maior chave: " + tree.maiorChave());
+
+    System.out.println("-------------------------------------------------------\n");
+
+    System.out.println("Menor chave da sub-árvore direita: " + tree.menorChaveAD());
+
+    System.out.println("-------------------------------------------------------\n");
+
+    System.out.println("Maior chave da sub-árvore esquerda: " + tree.maiorChaveAE());
+
+    System.out.println("-------------------------------------------------------\n");
+
+    System.out.println("Quantidade de nós recursivo: " + tree.quantNodesRec());
+
+    System.out.println("-------------------------------------------------------\n");
+
+    System.out.println("Quantidade de nós iterativo: " + tree.quantNodesIte());
+
+    System.out.println("-------------------------------------------------------\n");
+
+    System.out.println("Quantidade de folhas recursivo: " + tree.quantFolhasRec());
+
+    System.out.println("-------------------------------------------------------\n");
+
+    System.out.println("Quantidade de folhas iterativo: " + tree.quantFolhasIte());
+
+    System.out.println("-------------------------------------------------------\n");
+
+    System.out.println("Quantidade de nós internos iterativo: " + tree.quantIternos());
+
+    System.out.println("-------------------------------------------------------\n");
+
+    System.out.println("Quantidade de nós internos recursivo: " + tree.contarNosInternosRec());
+
+    System.out.println("-------------------------------------------------------\n");
+
+    tree.percorrerPorNivel();
+
+    System.out.println("-------------------------------------------------------\n");
+
+    tree.codigos();
+
+    System.out.println("-------------------------------------------------------\n");
+
+    System.out.println("Profundidade do nó " + 16 + " é: " + tree.profundidadeRec(16));
   }
 }
--- tree/BinaryTree.java
+++ tree/BinaryTree.java
@@ -1,155 +1,544 @@
 package tree;
 
-public class BinaryTree<E extends Comparable<E>> {
-  public class Node {
-    E data;
-    Node left;
-    Node right;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
-    public Node(E data) {
-      this.data = data;
-      this.left = null;
-      this.right = null;
-    }
-
-    public E getData(){
-      return this.data;
-    }
-
-    public Node getLeft(){
-      return this.left;
-    }
-
-    public Node getRight(){
-      return this.right;
-    }
-  }
-
+public class BinaryTree {
   private Node root;
 
-  public Node getRoot(){
-    return this.root;
-  }
-
   public BinaryTree() {
-    this.root = null;
+    root = null;
   }
 
-  public Node insert(E data) {
-    if (root == null) {
-      root = new Node(data);
-    }
+  // Métodos de busca recursiva
+  // -----------------------------------------------------------------------------------------------------------
 
-    return insertRec(root, data);
+  public Node search(int key) {
+    return search(key, root);
   }
 
-  private Node insertRec(Node current, E data) {
-    if (current == null) {
-      current = new Node(data);
-    }
+  private Node search(int key, Node root) {
+    if (root == null || root.key == key)
+      return root;
 
-    if (data.compareTo(current.data) < 0) {
-      current.left = insertRec(current.left, data);
-    } else if (data.compareTo(current.data) > 0) {
-      current.right = insertRec(current.right, data);
-    }
-
-    return current;
+    if (key < root.key)
+      return search(key, root.left);
+    else
+      return search(key, root.right);
   }
 
-  public Node search(E data) {
+  // Método para inserir recursivamente
+  // -----------------------------------------------------------------------------------------------------------
+
+  public void insert(int valor) {
+    root = insert(valor, root);
+  }
+
+  private Node insert(int key, Node root) {
+    if (root == null)
+      return root;
+
+    if (key < root.key)
+      root.left = insert(key, root.left);
+    else if (key > root.key)
+      root.right = insert(key, root.right);
+
+    return root;
+  }
+
+  // Métodos recursivos para percorrer a árvore
+  // -----------------------------------------------------------------------------------------------------------
+
+  public void ordemRec() { // método público para exibir em ordem
+    System.out.print("Percurso em Ordem Recusivo: ");
+    this.ordemRec(root);
+    System.out.println('\n');
+  }
+
+  private void ordemRec(Node root) { // método privado para exibir em ordem
+    if (root != null) {
+      this.ordemRec(root.left);
+      System.out.print(root.key + " ");
+      this.ordemRec(root.right);
+    }
+  }
+
+  public void preOrdemRec() {
+    System.out.print("Percurso em Pré-Ordem Recusivo: ");
+    preOrdemRec(root);
+    System.out.println('\n');
+  }
+
+  private void preOrdemRec(Node root) {
+    if (root != null) {
+      System.err.print(root.key + " ");
+      preOrdemRec(root.left);
+      preOrdemRec(root.right);
+    }
+  }
+
+  public void posOrdemRec() {
+    System.out.print("Percurso em Pós-Ordem Recusivo: ");
+    posOrdemRec(root);
+    System.out.println('\n');
+  }
+
+  private void posOrdemRec(Node root) {
+    if (root != null) {
+      posOrdemRec(root.left);
+      posOrdemRec(root.right);
+      System.err.print(root.key + " ");
+    }
+  }
+
+  // Métodos iterativos para percorrer a árvore
+  // -----------------------------------------------------------------------------------------------------------
+
+  public void ordemIte() { // método público para exibir em ordem
+    System.out.print("Percurso em Ordem Iterativo: ");
+    this.ordemIte(root);
+    System.out.println('\n');
+  }
+
+  private void ordemIte(Node root) { // método privado para exibir em ordem
+    if (root == null)
+      return;
+
+    Stack<Node> stack = new Stack<>();
     Node current = root;
 
-    while (current != null) {
-      if (data.compareTo(current.data) == 0) {
-        return current; // Encontrou o nó
-      } else if (data.compareTo(current.data) < 0) {
-        current = current.left; // Vá para a subárvore esquerda
-      } else {
-        current = current.right; // Vá para a subárvore direita
+    while (current != null || !stack.isEmpty()) {
+      while (current != null) {
+        stack.push(current);
+        current = current.left;
       }
-    }
-    
-    return null; // Não encontrou o nó
-  }
 
-  private Node searchRecursive(Node current, E data) {
-    if (current == null || data.compareTo(current.data) == 0) {
-      return current;
-    }
-
-    if (data.compareTo(current.data) < 0) {
-      return searchRecursive(current.left, data);
-    } else {
-      return searchRecursive(current.right, data);
+      current = stack.pop();
+      System.out.print(current.key + " ");
+      current = current.right;
     }
   }
 
-  // Para iniciar a busca a partir da raiz:
-  public Node searchRecursive(E data) {
-    return searchRecursive(root, data);
+  public void preOrdemIte() {
+    System.out.print("Percurso em Pré-Ordem Iterativo: ");
+    preOrdemIte(root);
+    System.out.println('\n');
   }
 
-  public Node deleteRecursive(E data) {
-    if (root == null) {
-      return null;
-    } else if (data.compareTo(root.data) == 0) {
+  private void preOrdemIte(Node root) {
+    if (root == null)
+      return;
+
+    Stack<Node> stack = new Stack<>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+      Node current = stack.pop();
+      System.out.print(current.key + " ");
+
+      if (current.right != null)
+        stack.push(current.right);
+      if (current.left != null)
+        stack.push(current.left);
+    }
+  }
+
+  public void posOrdemIte() {
+    System.out.print("Percurso em Pós-Ordem Iterativo: ");
+    posOrdemIte(root);
+    System.out.println('\n');
+  }
+
+  private void posOrdemIte(Node root) {
+    if (root == null)
+      return;
+
+    Stack<Node> stack = new Stack<>();
+    Stack<Node> stackOut = new Stack<>();
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+      Node current = stack.pop();
+      stackOut.push(current);
+
+      if (current.left != null)
+        stack.push(current.left);
+      if (current.right != null)
+        stack.push(current.right);
+    }
+
+    while (!stackOut.isEmpty()) {
+      Node current = stackOut.pop();
+      System.out.print(current.key + " ");
+    }
+  }
+
+  // métodos para remover recursivamente
+  // -----------------------------------------------------------------------------------------------------------
+
+  public void remove(int key) {
+    remove(key, root);
+  }
+
+  private Node remove(int key, Node root) {
+    if (root == null)
       return root;
-    }
 
-    return deleteRecursive(root, data);
-  }
-
-  private Node deleteRecursive(Node arv, E valor) {
-    if (arv == null) {
-      return null;
-    }
-
-    if (valor.compareTo(arv.data) < 0) {
-      arv.left = deleteRecursive(arv.left, valor);
-    } else if (valor.compareTo(arv.data) > 0) {
-      arv.right = deleteRecursive(arv.right, valor);
-    } else {
-      // Caso 1: Nó sem filhos ou com um filho (tratado como casos 2 e 3)
-      if (arv.left == null) {
-        return arv.right;
-      } else if (arv.right == null) {
-        return arv.left;
+    if (key < root.key)
+      root.left = remove(key, root.left);
+    else if (key > root.key)
+      root.right = remove(key, root.right);
+    else {
+      if (root.right == null && root.left == null)
+        root = null;
+      else if (root.left == null) {
+        root = root.right;
+      } else if (root.right == null) {
+        root = root.left;
       } else {
-        // Caso 3: Nó com dois filhos
-        // Encontrar o sucessor na subárvore direita
-        Node temp = arv.right;
-        while (temp.left != null) {
-          temp = temp.left;
-        }
-        // Substituir o valor do nó atual pelo valor do sucessor
-        arv.data = temp.data;
-        // Remover o sucessor da subárvore direita
-        arv.right = deleteRecursive(arv.right, temp.data);
+        Node aux = minorKeyRoot(root.right);
+        root.key = aux.key;
+        root.right = remove(aux.key, root.right);
       }
     }
 
-    return arv; // Retornar o nó atual modificado após a remoção
+    return root;
   }
 
-  public void displayOrder() {
-    displayOrder(root);
-    System.out.println("\n-------------------------------------------");
+  // Método recursivo para buscar a menor chave da sub-árvore direita
+  // -----------------------------------------------------------------------------------------------------------
+
+  private Node minorKeyRoot(Node root) {
+    if (root == null) // nunca vai entrar aqui, mas... Por segurança....
+      return null;
+
+    Node aux = root;
+
+    while (aux.left != null)
+      aux = aux.left;
+
+    return aux;
   }
 
-  private void displayOrder(Node node) {
-    if (node != null) {
-      displayOrder(node.left); // Visitando subárvore esquerda
-      System.out.print(node.data + " "); // Visita o próprio nó
-      displayOrder(node.right); // Visitando subárvore direita
+  // Método recursivo para calcular a altura
+  // -----------------------------------------------------------------------------------------------------------
+
+  public int height() {
+    return height(root);
+  }
+
+  private int height(Node root) {
+    int a, b;
+
+    if (root == null)
+      return -1;
+
+    a = height(root.left);
+    b = height(root.right);
+
+    if (a > b)
+      return a + 1;
+    else
+      return b + 1;
+  }
+
+  // Métodos da questão 11 da primeira lista
+  // A menor chave da árvore binária de busca.
+  // -----------------------------------------------------------------------------------------------------------
+
+  public int menorChave() {
+    return menorChave(root);
+  }
+
+  private int menorChave(Node root) {
+    if (root == null)
+      return -1;
+
+    Node aux = root;
+    while (aux.left != null)
+      aux = aux.left;
+
+    return aux.key;
+  }
+
+  // maior chave da árvore binária de busca
+  // -----------------------------------------------------------------------------------------------------------
+
+  public int maiorChave() {
+    return maiorChave(root);
+  }
+
+  private int maiorChave(Node root) {
+    if (root == null)
+      return -1;
+
+    Node aux = root;
+    while (aux.right != null)
+      aux = aux.right;
+
+    return aux.key;
+  }
+
+  // A menor chave da subárvore direta de um nó da árvore binária de busca
+  // -----------------------------------------------------------------------------------------------------------
+
+  public int menorChaveAD() {
+    return menorChaveAD(root.right);
+  }
+
+  private int menorChaveAD(Node root) {
+    if (root == null)
+      return -1;
+
+    Node aux = root;
+
+    while (aux.left != null) {
+      aux = aux.left;
+    }
+
+    return aux.key;
+  }
+
+  // A maior chave da subárvore esquerda de um nó da árvore binária de busca
+  // -----------------------------------------------------------------------------------------------------------
+
+  public int maiorChaveAE() {
+    return maiorChaveAE(root.left);
+  }
+
+  private int maiorChaveAE(Node root) {
+    if (root == null)
+      return -1;
+
+    Node aux = root;
+
+    while (aux.right != null) {
+      aux = aux.right;
+    }
+
+    return aux.key;
+  }
+
+  // A quantidade de nós recursivo
+  // -----------------------------------------------------------------------------------------------------------
+
+  public int quantNodesRec() {
+    return quantNodesRec(root);
+  }
+
+  private int quantNodesRec(Node root) {
+    if (root == null)
+      return 0;
+    else
+      return 1 + quantNodesRec(root.left) + quantNodesIte(root.right);
+  }
+
+  // A quantidade de nós iterativo
+  // -----------------------------------------------------------------------------------------------------------
+
+  public int quantNodesIte() {
+    return quantNodesIte(root);
+  }
+
+  private int quantNodesIte(Node root) {
+    if (root == null)
+      return 0;
+
+    Stack<Node> stack = new Stack<>();
+    stack.push(root);
+    int cont = 0;
+
+    while (!stack.isEmpty()) {
+      Node current = stack.pop();
+      cont++;
+
+      if (current.right != null)
+        stack.push(current.right);
+      if (current.left != null)
+        stack.push(current.left);
+    }
+    return cont;
+  }
+
+  // Quantidade de folhas
+  // -----------------------------------------------------------------------------------------------------------
+
+  public int quantFolhasRec() {
+    return quantFolhasRec(root);
+  }
+
+  private int quantFolhasRec(Node root) {
+    if (root == null)
+      return 0;
+
+    if (root.left == null && root.right == null)
+      return 1;
+    else
+      return quantFolhasRec(root.left) + quantFolhasRec(root.right);
+  }
+
+  public int quantFolhasIte() {
+    return quantFolhasIte(root);
+  }
+
+  // iterativo
+
+  private int quantFolhasIte(Node root) {
+    if (root == null)
+      return 0;
+
+    Stack<Node> stack = new Stack<>();
+    stack.push(root);
+    int cont = 0;
+
+    while (!stack.isEmpty()) {
+      Node current = stack.pop();
+      if (current.left == null && current.right == null)
+        cont++;
+
+      if (current.right != null)
+        stack.push(current.right);
+      if (current.left != null)
+        stack.push(current.left);
+    }
+    return cont;
+  }
+
+  // Quantidade de nós internos
+  // -----------------------------------------------------------------------------------------------------------
+
+  public int quantIternos() {
+    return quantInternos(root);
+  }
+
+  private int quantInternos(Node root) {
+    if (root == null)
+      return 0;
+
+    Stack<Node> stack = new Stack<>();
+    stack.push(root);
+    int cont = 0;
+
+    while (!stack.isEmpty()) {
+      Node current = stack.pop();
+      if (current.left != null || current.right != null)
+        cont++;
+
+      if (current.right != null)
+        stack.push(current.right);
+      if (current.left != null)
+        stack.push(current.left);
+    }
+    return cont;
+  }
+
+  public int contarNosInternosRec() {
+    return contarNosInternosRec(root);
+  }
+
+  private int contarNosInternosRec(Node root) {
+    if (root == null)
+      return 0;
+    if (root.left != null || root.right != null)
+      return contarNosInternosRec(root.left) + contarNosInternosRec(root.right) + 1;
+    else
+      return 0;
+  }
+
+  // Percorrer a árvore por largura (ou por nível)
+  // -----------------------------------------------------------------------------------------------------------
+
+  public void percorrerPorNivel() {
+    System.out.print("Percurso em por nível: ");
+    percorrerPorNivel(root);
+    System.out.println('\n');
+  }
+
+  private void percorrerPorNivel(Node root) {
+    if (root == null)
+      return;
+
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(root);
+
+    while (!queue.isEmpty()) {
+      Node current = queue.poll();
+      System.out.print(current.key + " ");
+      if (current.left != null)
+        queue.add(current.left);
+      if (current.right != null)
+        queue.add(current.right);
     }
   }
 
-    /**
-     * @param root the root to set
-     */
-    public void setRoot(Node root) {
-        this.root = root;
-    }
+  // Questão 12 da lista
+  // -----------------------------------------------------------------------------------------------------------
 
+  public void codigos() {
+    codigos(root, "");
+  }
+
+  private void codigos(Node root, String codigoAtual) {
+    if (root == null)
+      return;
+
+    root.codigo = codigoAtual;
+    System.out.println("Codigo do nó: " + root.key + " - é: " + root.codigo);
+
+    if (root.left != null)
+      codigos(root.left, codigoAtual + "0");
+    if (root.right != null)
+      codigos(root.right, codigoAtual + "1");
+  }
+
+  // Questão 13 da lista
+  // -----------------------------------------------------------------------------------------------------------
+
+  public int profundidadeIte(int codigo) {
+    Node aux = search(codigo);
+    if (aux == null)
+      return -1;
+
+    return profundidadeIte(root, codigo);
+  }
+
+  private int profundidadeIte(Node root, int codigo) {
+    if (root == null || root.key == codigo)
+      return 0;
+
+    int cont = 0;
+    Node aux = root;
+    while (codigo != aux.key) {
+      if (aux.left != null && codigo < aux.key) {
+        aux = aux.left;
+        cont++;
+      } else if (aux.right != null && codigo > aux.key) {
+        aux = aux.right;
+        cont++;
+      }
+    }
+    return cont;
+  }
+
+  public int profundidadeRec(int codigo) {
+    Node aux = search(codigo);
+    if (aux == null)
+      return -1;
+
+    return profundidadeRec(root, codigo);
+  }
+
+  // pronfunidade recursiva
+  private int profundidadeRec(Node root, int codigo) {
+    if (root == null)
+      return 0;
+
+    if (codigo < root.key)
+      return profundidadeRec(root.left, codigo) + 1;
+    else if (codigo > root.key)
+      return profundidadeRec(root.right, codigo) + 1;
+    else
+      return 0;
+  }
 }
